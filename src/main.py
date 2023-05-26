7c0545233d5f05d381d67a15878c4c115432c5cd
Mikhail Tugulkhanov <mtugulkhanov@mail.ru>
2023-05-26 11:59:46 +0800

    add Hello World

--- src/main.py
+++ src/main.py
@@ -1,9 +1,2 @@
-•	def new_sum(***nums**):
-•	#             |
-•	#  Запакованные аргументы
-•	  sum = 0
-•	  for n in nums:
-•	    sum += n
-•	  return sum
-•	
-•	print(new_sum(1, 1, 1, 1, 1)
+print("Hello World")
+)
